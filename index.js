4e63d00e6854ea9afe0098359ae7a467d551f98b
arpan-403 <assanu403@gmail.com>
2024-04-26 01:00:05 +0530

    first

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express=require("express");
+const app=express();
+var cors = require('cors')
+const path = require("path");
+app.use(cors())
+const PORT=process.env.PORT || 5000
+
+
+const login=require("./Routes/login");
+const isAuthenticate=require("./Routes/isAuthenticate");
+const register=require("./Routes/register");
+const uploads=require("./Routes/uploads");
+const uploadedByMe=require("./Routes/uploadedByMe");
+const share=require("./Routes/share");
+const shareWithMe=require("./Routes/shareWithMe");
+const shareByMe=require("./Routes/shareByMe");
+// app.use("/uploads",express.static("uploads"));
+// app.use(express.static(path.join(__dirname, 'build')));
+app.use(login);
+app.use(isAuthenticate);
+app.use(register);
+app.use(uploads);
+app.use(uploadedByMe);
+app.use(share);
+app.use(shareWithMe);
+app.use(shareByMe);
+// app.get("/",(req,res)=>{
+//     res.send("in server");
+// })
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
+app.get("/", (req, res) => { app.use(express.static(path.resolve(__dirname, "frontend", "build"))); res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")); });
+app.get("/uploads", (req, res) => { app.use(express.static(path.resolve(__dirname, "frontend", "build"))); res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")); });
+app.get("/sharedByYou", (req, res) => { app.use(express.static(path.resolve(__dirname, "frontend", "build"))); res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")); });
+app.get("/sharedWithYou", (req, res) => { app.use(express.static(path.resolve(__dirname, "frontend", "build"))); res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")); });
+app.listen(PORT,()=>console.log("server is connected...."));
